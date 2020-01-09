6c3fa6ba9f7d17fbae00431e9023dc44a2d8db43
Zhenya Warshavsky <zwarshavsky@gmail.com>
2020-01-09 09:36:03 -0800

    committing changes

---
+++ data acquisition/env_vars.py
@@ -0,0 +1,7 @@
+SPOTIFYUSERNAME = 'basslaughter' #your spotify username
+CLIENT_ID = '63594c9b2f99411a8cbd18df04851fc4' #set at your developer account
+CLIENT_SECRET = '096168b2bd1f4378ae410726955c9ed8' #set at your developer account
+REDIRECT_URI = 'http://google.com/' #set at your developer account, usually "http://localhost:8000"
+SCOPE = 'user-library-read' # or else
+
+GENIUS_CLIENT_ACCESS_TOKEN = 'p0TX__gy-p5bD4UG4D0ASZ3tiLvVNqlD0iStYgOPR3OPakUewNwHlMBkodfkCv1V'
